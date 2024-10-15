b1355097d09dd21498c075511f9614489706f66e
Ayushi Jhinjhore <Ayushijhinjhore12@gmail.com>
2024-10-16 00:32:24 +0530

    base_node_template

---
+++ folderStructure2/src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+const User = require('../models/user.models')
+
+function createUser(req, res){
+    //work with your User model
+    console.log('trial');
+    res.send('working')
+}
+
+module.exports = {createUser}
